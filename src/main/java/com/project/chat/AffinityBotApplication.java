d8d6117bc403489a5bda35635175f0dc3785fc68
ankitkamal <officiallyankit999@gmail.com>
2024-12-22 01:38:40 +0530

    Basic Initializr app in working state

---
+++ src/main/java/com/project/chat/AffinityBotApplication.java
@@ -0,0 +1,13 @@
+package com.project.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AffinityBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AffinityBotApplication.class, args);
+    }
+
+}
